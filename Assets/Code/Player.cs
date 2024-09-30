4d009743c0c326273082cf96b332413d4e97a1a0
Xinu <66197586+Jinu219@users.noreply.github.com>
2024-09-30 16:49:13 +0900

    [Feature] 플레이어 이동 구현
    후에 스피드등의 부가 설정을 추가할 예정입니다.

--- Assets/Code/Player.cs
+++ Assets/Code/Player.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	public Vector2 inputVec;
+	public float speed;
+		
+	Rigidbody2D rigid;
+		
+	void Start()
+	{
+		rigid = GetComponent<Rigidbody2D>();
+	}
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	void Update()
+	{
+		inputVec.x = Input.GetAxisRaw("Horizontal");
+		inputVec.y = Input.GetAxisRaw("Vertical");
+	}
+		
+	void FixedUpdate()
+	{
+		Vector2 nextVec = inputVec.normalized * speed * Time.fixedDeltaTime;
+		rigid.MovePosition(rigid.position + nextVec);
+	}
 }
