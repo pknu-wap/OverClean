d66becd0b5752416fc662452ebef70a90cb9546f
Xinu <66197586+Jinu219@users.noreply.github.com>
2024-10-02 15:30:03 +0900

    [Chore] 코드 주석 추가

--- Assets/Script/MainScreenEvent.cs
+++ Assets/Script/MainScreenEvent.cs
@@ -7,14 +7,18 @@ public class MainScreenEvent : MonoBehaviour
 {
     public void SceneMove()
     {
+        // GameLobby 씬으로 이동
         SceneManager.LoadScene("GameLobby");
     }
 
     public void Exit()
     {
+        // #if 키워드를 사용하여 플랫폼별로 다르게 실행하는 함수.
+        // 에디터와 프로그램 실행을 구분.
         #if UNITY_EDITOR
             UnityEditor.EditorApplication.isPlaying = false;
         #else
+            // 어플리케이션 종료
             Application.Quit();
         #endif
     }
