dba909b00af1b45236615152a939c92b40fa6e81
Xinu <66197586+Jinu219@users.noreply.github.com>
2024-10-06 14:28:55 +0900

    [Style] Event 스크립트 클래스 명 변경
    Event Class 코드 파일을 만들고 나서, "Event"로 파일 명을 변경 했는데, 코드 내에서 클래스 이름을 변경하지 않았음.

--- Assets/Script/Event.cs
+++ Assets/Script/Event.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MainScreenEvent : MonoBehaviour
+public class Event : MonoBehaviour
 {
     public void MoveToLobby()
     {
